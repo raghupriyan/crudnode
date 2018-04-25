59b69e9acb23d53c6711192fe558d8cae605b8df
RaghuPriyan <priyan.06@gmail.com>
2018-04-26 03:15:21 +0530

    "ALL"

---
+++ server.js
@@ -0,0 +1,61 @@
+console.log('May node be with you')
+const express = require('express');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+var app = express();
+
+var db;
+var mongourl = 'mongodb://curduser:curdpassword@ds259089.mlab.com:59089/firstcurd';
+app.set('view engine', 'ejs')
+
+
+/*app.listen(3000, function() {
+	console.log('listening in 3000')
+})
+
+//app.get('/', (request, response)=>{
+//response.send('HelloWorld')
+//})
+
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+//app.post('/quotes',(req, res) => {
+	//res.send(req.body)
+//})*/
+/*app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})*/
+
+MongoClient.connect(mongourl, (err, client) => {
+	if(err) return console.log(err)
+	db = client.db('firstcurd')
+	app.listen(3000, () => {
+		
+		console.log('listening on 3000')
+		
+	})
+	
+	app.use(bodyParser.urlencoded({extended: true}))
+	
+	app.post('/quotes', (req, res) => {
+		db.collection('quotes').save(req.body, (err, result) =>{
+			if(err) return console.log(err)
+			
+		console.log('saved to database')
+		res.redirect('/')
+		})
+		
+	})
+	
+	app.get('/', (req, res) => {
+		//res.sendFile(__dirname + '/index.html')
+			db.collection('quotes').find().toArray((err,results) => {
+				if(err) return console.log(err)
+					
+			 res.render('index.ejs', {quotes: results})
+			//console.log(results)
+		})
+	})
+})
